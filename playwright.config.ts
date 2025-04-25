fb10151ea44f63b6815a68a17575d818d1f8edc4
AlexKomanov <akomanov88@gmail.com>
2025-04-25 10:35:12 +0300

    adding timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 120 * 1000,
+  globalTimeout: 500 * 1000,
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
